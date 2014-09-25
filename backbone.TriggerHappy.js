f50b506b9c954474bc56cf294da7ec384433b13e
drwrchrds <drwrchrds@gmail.com>
2014-09-25 02:30:27 -0700

    fix className issue

--- backbone.TriggerHappy.js
+++ backbone.TriggerHappy.js
@@ -2,9 +2,9 @@ var reTrigger = Backbone.Events.trigger
 
 Backbone.Events.trigger = function(name) {
   // TRIGGER LOGGER
-  // logging the className and the name of the trigger event
+  // logging the class name and the name of the trigger event
   var id = this.id || 'n/a'
-  console.log(this.className + ' (' + id + "): " + name)
+  console.log(this.triggerClassName + ' (' + id + "): " + name)
 
   return reTrigger.apply(this, arguments)
 }
@@ -14,7 +14,7 @@ _.extend(Backbone.Collection.prototype, Backbone.Events)
 _.extend(Backbone.Router.prototype, Backbone.Events)
 _.extend(Backbone.View.prototype, Backbone.Events)
 
-Backbone.Model.prototype.className = 'Model'
-Backbone.Router.prototype.className = 'Router'
-Backbone.Collection.prototype.className = 'Collection'
-Backbone.View.prototype.className = 'View'
+Backbone.Model.prototype.triggerClassName = 'Model'
+Backbone.Router.prototype.triggerClassName = 'Router'
+Backbone.Collection.prototype.triggerClassName = 'Collection'
+Backbone.View.prototype.triggerClassName = 'View'
